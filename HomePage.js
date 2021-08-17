fde21ea07f9016d22564de0b2dab23f26765692d
dusanfedor <76064384+dusanfedor@users.noreply.github.com>
2021-08-17 14:35:30 +0200

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "../pages.css";
+function HomePage() {
 let d = new Date();
 if (d.getHours() > 5 && d.getHours() < 18) document.write("Dobrý den");
 else document.write("Dobrý večer");
 
-function HomePage() {
-  return <div className="calculator"></div>;
+
+  return (
+  <div className="calculator"></div>
+    );
 }
 export default HomePage;
